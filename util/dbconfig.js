1b67b8b315269e854b98da283fb0177cefd85a57
jesseice <1642079960@qq.com>
2022-05-08 03:38:24 +0800

    修改数据库配置

--- util/dbconfig.js
+++ util/dbconfig.js
@@ -16,20 +16,11 @@ module.exports = {
     let pool = mysql.createPool(this.config)
     pool.getConnection((err,conn)=>{
       if(err) return console.log('连接失败');
-
       // 事件驱动回调
       conn.query(sql, sqlArr, function (error, results, fields) {
-        // When done with the connection, release it.
         conn.release()
         callBack(error, results)
-
-        // Handle error after the release.
-        // if (error) throw error
-
-        // Don't use the connection here, it has been returned to the pool.
       })
-      // 释放连接
-      // conn.release()
     })
   },
   // promise 回调
@@ -48,7 +39,6 @@ module.exports = {
               resolve(data)
             }
           })
-          // conn.release()
         }
       })
     }).catch((err)=>{
